d4ad3039edcce3bb9fe32e261b2335dd8706b9d4
nikita <kumbhareshubham9@gmail.com>
2021-10-11 19:08:07 +0530

    News App

---
+++ pages/eom.js
@@ -0,0 +1,15 @@
+export default function EOM() {
+  return (
+    <>
+      <div className="page-container">
+        <div>
+          <h1>Employees of the Month</h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export async function getServerSideProps(context) {
+    
+}
