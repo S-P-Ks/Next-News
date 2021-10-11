7cb407127a35388be487e1fc35008294779d5ded
nikita <kumbhareshubham9@gmail.com>
2021-10-11 19:16:10 +0530

    News App1

--- pages/eom.js
+++ pages/eom.js
@@ -1,9 +1,10 @@
-export default function EOM() {
+export default function EOM({ data }) {
   return (
     <>
       <div className="page-container">
         <div>
           <h1>Employees of the Month</h1>
+          {data}
         </div>
       </div>
     </>
@@ -11,5 +12,15 @@ export default function EOM() {
 }
 
 export async function getServerSideProps(context) {
-    
+  const data = await fetch(
+    "https://my-json-server.typicode.com/S-P-Ks/Next-News"
+  );
+
+  console.log(context);
+
+  return {
+    props: {
+      data: data,
+    },
+  };
 }
